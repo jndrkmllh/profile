b3ed0471912881c76d8e0c1e9a7fdecc3a43da78
Jendrik Mallah <jm.orders+lewagon@outlook.com>
2025-01-09 16:00:59 +0100

    Update personal information and add new project details; improved layout and formatting across multiple pages.
    Signed-off-by: Jendrik Mallah <jm.orders+lewagon@outlook.com>

--- _site/lib/index.js
+++ _site/lib/index.js
@@ -1,14 +1,9 @@
-console.log("hello from JS")
-
 const myLabels = document.querySelectorAll('.lbl-toggle');
-
-// console.log(myLabels)
 
 Array.from(myLabels).forEach(label => {
   label.addEventListener('click', e => {
     e.preventDefault();
 
-    // console.dir(label.previousElementSibling.checked)
     let checkbox = label.previousElementSibling
     checkbox.checked = !checkbox.checked;
   });
@@ -17,28 +12,23 @@ Array.from(myLabels).forEach(label => {
 const projectLinks = document.querySelectorAll(".project-link");
 const titles = document.querySelectorAll(".title");
 
-// console.log(projectLinks)
-// console.log(titles)
-
 Array.from(projectLinks).forEach(link => {
-  console.dir(link);
-
   const url = link.href;
 
   fetch(url)
   .then(response => {
       if (response.ok) {
-          console.log("Heroku app is online.");
+          // console.log(`Project is online. ${url}`);
           // Handle the app being online (e.g., update the UI accordingly)
           titles.forEach(title => {
             if (link.ariaLabel.toLowerCase() === title.innerText.toLowerCase()) {
-              title.nextElementSibling.classList.add("success");
-              title.nextElementSibling.innerText = "online";
               title.nextElementSibling.classList.remove("failed");
+              title.nextElementSibling.innerText = "online";
+              title.nextElementSibling.classList.add("success");
             }
           });
       } else {
-          console.log("Heroku app is offline or returned an error.");
+          // console.log(`Project is offline or returned an error. ${url}`);
           // Handle the app being offline or returning an error
           titles.forEach(title => {
             if (link.ariaLabel.toLowerCase() === title.innerText.toLowerCase()) {
@@ -50,7 +40,7 @@ Array.from(projectLinks).forEach(link => {
       }
   })
   .catch(error => {
-      console.log("Failed to reach Heroku app. It might be offline.", error);
+      // console.log("Failed to reach Project. It might be offline.", error);
       // Handle the app being unreachable (e.g., update the UI to show the app is offline)
       titles.forEach(title => {
         if (link.ariaLabel.toLowerCase() === title.innerText.toLowerCase()) {
